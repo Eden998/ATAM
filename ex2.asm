444ec018e7c02980fedd303de661482d1d59b835
Eden <eden33335@gmail.com>
2022-11-17 21:27:48 +0000

    finished ex2

--- ex2.asm
+++ ex2.asm
@@ -3,18 +3,28 @@
 .section .text
 _start:
 #your code here
-	movl (num), %ecx
-	js _neg
 	movl $0, %ecx
+	movl (num), %ebx
+	testl %ebx, %ebx
+	js _neg_init
 _pos:
 	movb source(, %ecx, 1), %al
 	movb %al, destination(, %ecx, 1)
 	inc %ecx
-	cmpl %ecx, num
+	cmpl %ecx, %ebx
 	jne _pos
 	jmp _end
+
+_neg_init:
+	imull $-1, %ebx
+	decl %ebx
+
 _neg:
-
-
-
+	movb source(, %ecx, 1), %al
+	movb %al, destination(, %ebx, 1)
+	decl %ebx
+	incl %ecx
+	cmpl $-1, %ebx
+	jne _neg
+	jmp _end
 _end:
